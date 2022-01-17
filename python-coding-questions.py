ef3698c846c195e315d7e818e6e311cbe5fb7a49
LetMeBake <95760213+rodrichr29@users.noreply.github.com>
2022-01-16 23:39:03 -0600

    Update python-coding-questions.py

--- python-coding-questions.py
+++ python-coding-questions.py
@@ -1,30 +1,50 @@
 # Question 1
 def hello_name(user_name):
-    user_name=input("Type in your username!: ")
-    print("Hello "+user_name+"!")
+    user_name = input("Type in your username!: ")
+    print("Hello " + user_name + "!")
 hello_name("user_name")
 print(hello_name)
 
+
+
 # Question 2
 def first_odds():
-    start,end=1,100
-    for num in range(start,end+1):
-        if num%2!=0:
-            print(num,end=" ")
+    list=[*range(1,100)]
+    for numbers in list:
+        if numbers % 2:
+            print(numbers, end = " ")
 first_odds()
 print(first_odds)
 
+
+
 # Question 3
 def max_num_in_list(a_list):
-    max_num=a_list[0]
-    for i in a_list:
-        if i > max_num:
-            max_num=i
-    return max_num
+    a_list=[0.002, 0.010042, 0.2, 0.2001, 0.004012]
+    print(max(a_list))
 max_num_in_list("a_list")
-print(max_num_in_list([0.124,0.1,0.041,0.010043,0.240214,0.0000001]))
+print(max_num_in_list)
+
+
 
 # Question 4
 def is_leap_year(a_year):
-    
+    a_year = int(input("Input an year: "))
+    if a_year % 4 == 0 and a_year % 100 != 0:
+        print(a_year, "is a leap year.")
+    elif a_year % 400 == 0 and a_year % 100 == 0:
+        print(a_year, "is a leap year.")
+    else:
+        print(a_year, "is not a leap year")
 is_leap_year("a_year")
+print(is_leap_year)
+
+
+
+# Question 5
+def is_consecutive(a_list):
+    for i in range(1, len(a_list)):
+        if(a_list[i - 1] + 1 != a_list[i]):
+            return False
+    return True
+print(is_consecutive([1, 2, 8, 4, 5]))
